d0070ab5451eaa092b6a6489edf897edafbd2a90
EduuIX <eduardomoraisribeiro@hotmail.com>
2025-03-06 13:36:17 -0300

    feat(server): adicionar configuração inicial do backend e dependências
    - Criação do arquivo `package.json` para o backend do servidor
    - Definição do nome do projeto como `tropicalize-app`
    - Adição de dependências essenciais: `express`, `jsonwebtoken`, `prisma`, `mysql2`, `bcrypt`, `nodemailer`, entre outras
    - Configuração de scripts para inicialização e desenvolvimento do servidor
    
    chore(deps): atualizar pacotes do projeto
    
    - Atualização e inclusão de pacotes nas dependências do `node_modules`
    - Inclusão de pacotes relacionados a autenticação (`jsonwebtoken`, `jwt-decode`), manipulação de arquivos (`multer`), banco de dados (`prisma`, `mysql2`) e segurança (`bcrypt`, `dotenv`)
    - Atualização de pacotes internos usados por bibliotecas de terceiros

--- server/app.js
+++ server/app.js
@@ -15,12 +15,27 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://89.116.74.250:5173",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: function (origin, callback) {
+      // Permitir origens no array ou origem ausente (caso de clientes sem origin como Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
